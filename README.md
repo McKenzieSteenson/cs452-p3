95e2d900ae0280895860f3e933a3495429872204
mckenziesteenson <mckenziesteenson@u.boisestate.edu>
2022-02-15 23:30:30 -0700

    added pseudo code and 'functioning' functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cs452-p3
+
+Main idea: define 30 circular doubly linked lists:
+List 0 connects block headers, and each header
+represents a memory block whose size is 2^0=1 byte.
+It's kval is 0;
+List 2 connects block headers, and each header represents a memory block whose size is 2^2=4 bytes
--- buddy.c
+++ buddy.c
@@ -17,7 +17,7 @@ int buddy_init(void) {
 		return FALSE;
 	}
 	// for i 0->28
-	for(i=0; i <29; i++) {
+	for(i=0; i <=29; i++) {
 		// next <- &avail[i] //// &avail[i] is the address of avail[i]
 		avail[i].next = &avail[i];
 		// prev <- &avail[i]
@@ -30,15 +30,70 @@ int buddy_init(void) {
 	return TRUE;
 }
 
+/* allocates size bytes andreturns a pointer to the 
+* allocated memory. If the memory cannot be allocated
+* then buddy_malloc function should return null
+*/
 void *buddy_malloc(size_t size)
 {
+	int lgsize = 0;
+	int i;
+	int j;
+	struct block_header * left;
+	struct block_header * right; 
+ 
+	
 	// p -> (char *)p+headerSize
+
+	// (struct block_header *) ((char *) p1 + sizeof())
+
+	// to assign an int to a specific place in memory
+		// type *ptr = (type *)[some place in memory or a void pointer]
+
+	// XOR equation
+		// x = y XOR (1ULL<<k)
+
+	// stores ceiling log base 2 of size in lgsize
+	size = size-1;
+	while (size > 0) {
+		size = size>>1;
+		lgsize++;
+	}
+
+	j = 0;
+	for (i = lgsize; i <= 29; i++) {
+		left = (struct block_header *) ((char *) base + (((char *)size - (char *)base) ^1ULL<<avail[i].kval));
+
+		if (avail != NULL) {
+
+			break;
+		
+			while(j > lgsize) {
+				// split
+				// right side shift
+					// right = (struct block_header *) ((unsigned long long int) left ^(1ULL<<(i-1)));
+				right = (struct block_header *) ((unsigned long long int) left ^(1ULL<<(i-1)));
+				return left = left + 1;
+			}
+		
+		}
+	}
 	return NULL;
 }
 
+/* frees the memory space pointed to by ptr
+* if ptr is NULL, return
+*/
 void buddy_free(void *ptr) 
 {
+	struct block_header * p;
+
+	ptr = buddy_malloc(sizeof(ptr));
+
+	p = (struct block_header *) ((char *) ptr - sizeof(ptr));
 	// p -> (char *)p-headerSize
+
+	//// (struct block_header *) ((char *) p1 - sizeof())
 
 }
 
@@ -49,7 +104,7 @@ void printBuddyLists(void)
 	// List 5: head = 0x602138 --> [tag=1,kval=5,addr=0x199e020] --> head = 0x602138
 // List {list number}: head = {addres of head} --> [tag={block_header.tag}, kval={block_header.kval}, addr={&addressOfBlock} --> head = {address of head}]
 	int i;
-	for (i = 0; i < 29; i++) {
+	for (i = 0; i <= 29; i++) {
 		if (avail[i].tag == 1) {
 			printf("List %d: head = %p --> [tag= %d, kval %d, addr={}] --> head = %p \n", avail[i].kval, &avail[i], avail[i].tag, avail[i].kval, &avail[i]);
 		} else {
